4ad7d9fab52117175d841b922e9da555666fbd5c
leticiagouvea <letfgouvea@gmail.com>
2022-11-23 05:17:54 -0300

    feat: signInSchema created

--- src/models/authSchema.js
+++ src/models/authSchema.js
@@ -4,8 +4,13 @@ const signUpSchema = joi.object({
   firstname: joi.string().required(),
   lastname: joi.string().required(),
   email: joi.string().email().required(),
-  password: joi.string().required().min(8),
+  password: joi.string().required().min(8)
 });
+
+const signInSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required().min(8)
+})
 
 async function validateSignUp(req, res, next) {
   const { firstname, lastname, email, password } = req.body;
@@ -22,4 +27,19 @@ async function validateSignUp(req, res, next) {
   next();
 }
 
-export { validateSignUp };
+async function validateSignIn() {
+  const { email, password } = req.body;
+
+  const validation = signInSchema.validate(req.body, { abortEarly: false });
+
+  if (validation.error) {
+    const error = validation.error.details.map(detail => detail.message);
+    return res.status(422).send(error);
+  }
+
+  res.locals.user = { email, password };
+
+  next();
+}
+
+export { validateSignUp, validateSignIn };
