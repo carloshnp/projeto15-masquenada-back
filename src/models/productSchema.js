6a86c40b1e9b847d0fc067d14c67411582e84682
carloshnp <carloshnpwork@gmail.com>
2022-11-22 23:25:52 -0300

    fix: joi function when() replacing alternatives/conditional

--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -5,27 +5,25 @@ export const productSchema = joi.object({
   description: joi.string().required().min(20),
   price: joi.number().required(),
   quantity: joi.number().required(),
-  type: joi.string().required().valid("camisa", "chuteira", "bola"),
-  shirtColor: joi.alternatives().conditional("type", {
+  type: joi.string().valid("camisa", "chuteira", "bola").required(),
+  shirtColor: joi.when("type", {
     is: "camisa",
     then: joi.string().required(),
   }),
-  shirtSize: joi.alternatives().conditional("type", {
+  shirtSize: joi.when("type", {
     is: "camisa",
     then: joi.string().required().valid("PP", "P", "M", "G", "GG"),
   }),
-  bootColor: joi.alternatives().conditional("type", {
+  bootColor: joi.when("type", {
     is: "chuteira",
     then: joi.string().required(),
   }),
-  bootSize: joi.alternatives().conditional("type", {
+  bootSize: joi.when("type", {
     is: "chuteira",
     then: joi.number().required().valid(36, 37, 38, 39, 40, 41, 42, 43, 44),
   }),
-  ballYear: joi
-    .alternatives()
-    .conditional("type", {
-      is: "bola",
-      then: joi.number().required(),
-    }),
+  ballYear: joi.when("type", {
+    is: "bola",
+    then: joi.number().required(),
+  }),
 });
