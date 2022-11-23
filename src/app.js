33d07f25d4f86972f7dcc8f692182c6ba2a915eb
leticiagouvea <letfgouvea@gmail.com>
2022-11-23 01:08:07 -0300

    add connection string mongodb atlas

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
-import productRouter from './routes/productsRoutes.js'
+/* import productRouter from './routes/productsRoutes.js'; */
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(productRouter)
+app.get("/health", (req, res) => res.send("OK"));
+/* app.use(productRouter); */
 
-app.listen(5000, () => {
-  console.log("App running at port: 5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App running at port: ${port}`);
 });
