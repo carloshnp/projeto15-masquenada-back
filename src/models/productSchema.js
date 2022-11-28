5128fc807ecddfd53e397b9a98b5fde2f214464a
carloshnp <carloshnpwork@gmail.com>
2022-11-28 01:12:37 -0300

    feat: products selection arrays created

--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -10,22 +10,18 @@ export const productSchema = joi.object({
   type: joi.string().valid("camisa", "chuteira", "bola").required(),
   shirtColor: joi.when("type", {
     is: "camisa",
-    then: joi.string().required(),
+    then: joi.array().items(joi.string().valid('principal', 'reserva').required()),
   }),
   shirtSize: joi.when("type", {
     is: "camisa",
     then: joi.array().items(joi.string().valid("PP", "P", "M", "G", "GG").required()),
   }),
-  bootColor: joi.when("type", {
-    is: "chuteira",
-    then: joi.string().required(),
-  }),
   bootSize: joi.when("type", {
     is: "chuteira",
-    then: joi.number().required().valid(36, 37, 38, 39, 40, 41, 42, 43, 44),
+    then: joi.array().items(joi.number().valid(36, 37, 38, 39, 40, 41, 42, 43, 44).required()),
   }),
   ballYear: joi.when("type", {
     is: "bola",
-    then: joi.number().required(),
+    then: joi.number().valid(2022, 2018, 2014, 2010, 2006, 2002, 1998, 1994).required(),
   }),
 });
